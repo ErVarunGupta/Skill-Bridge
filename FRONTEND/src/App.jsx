cd42e3b2fc04a5281101be702b3638f6156aed9b
ErVarunGupta <varungupta0994@gmail.com>
2025-09-11 04:44:19 +0530

    frontend -authentication

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,11 +1,28 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import LandingPage from './layouts/LandingPage'
+import { MyContext } from './MyContext'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 function App() {
 
+  const providerValues = {
+
+  }
+
   return (
     <>
-      
+      <MyContext.Provider value = {providerValues}>
+        <Routes>
+            <Route path={"/"} element={<LandingPage/>}/>
+            <Route path={"/login"} element={<Login/>}/>
+            <Route path={"/register"} element={<Register/>}/>
+            <Route path={"/dashboard"} element={<Dashboard/>}/>
+        </Routes>
+      </MyContext.Provider>
     </>
   )
 }
