ca808f88bbaf1b43cdcf170aba419d6d82a12725
ErVarunGupta <varungupta0994@gmail.com>
2025-09-19 19:36:28 +0530

    rating feature update

--- BACKEND/src/controllers/feedbackController.js
+++ BACKEND/src/controllers/feedbackController.js
@@ -24,9 +24,15 @@ export const updateRating = async (req, res) => {
     }
 
     const newTotalReviews = profile.totalReviews + 1;
-    const newAverageRating = 0;
+    let newAverageRating = 0;
     if(newTotalReviews > 0){
       newAverageRating = (profile.averageRating * profile.totalReviews + rating) / newTotalReviews;
+    }
+
+    console.log(newAverageRating);
+
+    if (isNaN(newAverageRating)) {
+      newAverageRating = 0;
     }
 
     profile.averageRating = newAverageRating;
