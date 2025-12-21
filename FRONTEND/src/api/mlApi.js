75e94689aacfd0be949cb0034b126d128e47821d
ErVarunGupta <varungupta0994@gmail.com>
2025-12-21 20:21:49 +0530

    connect to env

--- FRONTEND/src/api/mlApi.js
+++ FRONTEND/src/api/mlApi.js
@@ -1,13 +1,14 @@
 import axios from "axios";
+const API_URL = import.meta.env.VITE_ML_URL;
 
 export const getTopUsers = async () => {
-    const url = "http://localhost:8080/api/ml/top-users";
+    const url = `${API_URL}/ml/top-users`;
   const response = await fetch(url);
   return response;
 };
 
 export const getRecommendations = async (userIndex) => {
-  const url = `http://localhost:8080/api/ml/recommend?userIndex=${userIndex}`;
+  const url = `${API_URL}/ml/recommend?userIndex=${userIndex}`;
   const response = await fetch(url);
   return response;
 };
