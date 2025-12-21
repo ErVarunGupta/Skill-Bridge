44787fbb1233db2aa8971289d1fad52fe37f5569
ErVarunGupta <varungupta0994@gmail.com>
2025-12-21 19:31:39 +0530

    python version update

--- BACKEND/src/ML_INTEGRATION/ml_service/app.py
+++ BACKEND/src/ML_INTEGRATION/ml_service/app.py
@@ -5,7 +5,8 @@ import numpy as np
 from functools import lru_cache
 from sklearn.metrics.pairwise import cosine_similarity
 
-from mongo_loader import load_profiles_df
+from ML_INTEGRATION.ml_service.mongo_loader import load_profiles_df
+
 from feature_pipeline import build_ranking_features
 from huggingface_hub import hf_hub_download
 
